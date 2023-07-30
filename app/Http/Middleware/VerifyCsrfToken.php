1d68576d512f794eabaad3fddb836eb520fe74b0
Huy Pham <huyredevil@gmail.com>
2023-07-30 11:17:09 +0700

    fix csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as Middleware;
 
 class VerifyCsrfToken extends Middleware
@@ -14,4 +15,20 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
+
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->headers->set('X-CSRF-TOKEN', csrf_token());
+        $response->headers->set('Access-Control-Allow-Headers', 'X-CSRF-TOKEN');
+
+        return $response;
+    }
 }
