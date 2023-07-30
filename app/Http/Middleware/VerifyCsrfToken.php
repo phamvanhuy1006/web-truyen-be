662695e36dc4abee8729a37a9f6f20bddbf909b7
Huy Pham <huyredevil@gmail.com>
2023-07-30 11:23:24 +0700

    fix csrf

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -15,20 +15,4 @@ class VerifyCsrfToken extends Middleware
     protected $except = [
         //
     ];
-
-    /**
-     * Handle the incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        $response = $next($request);
-        $response->headers->set('X-CSRF-TOKEN', csrf_token());
-        $response->headers->set('Access-Control-Allow-Headers', 'X-CSRF-TOKEN');
-
-        return $response;
-    }
 }
